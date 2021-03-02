d26d268134b93f3f678a2a3c219749307fbaff11
Mark <markjpoole@yahoo.com>
2021-03-02 17:02:35 +0000

    set ejs as view engine

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
   res.send("Hello");
-})
-
+}
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 })
