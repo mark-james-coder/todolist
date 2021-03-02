b059b2acaccd29ded364aef7a3751ea480646846
Mark <markjpoole@yahoo.com>
2021-03-02 17:53:16 +0000

    render type of day using server logic and list view

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res) {
-  res.send("Hello");
-}
+app.get("/", (req, res) => {
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  if (currentDay === 6 || currentDay === 0) {
+    day = "weekend";
+  } else {
+    day = "weekday";
+  }
+  res.render("list", {nameOfDay: day});
+
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 })
